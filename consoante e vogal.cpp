c5454b6fd9dfc9a6ec50ee7312f8798fef712c2b
john mendes <131309978+Johnw07@users.noreply.github.com>
2023-05-25 21:04:22 -0300

    Add files via upload

---
+++ consoante e vogal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+char vogal, consoante, a, e, i, o, u;
+	
+	
+		cout<<"digite uma letra \n";
+		cin>>vogal;
+		cout<<"digite outra letra \n";
+		cin>>consoante;
+		
+			if(vogal = 'a','e','i','o','u'){
+				cout<<"essa letra e uma vogal \n";
+				
+			}
+				else{
+					cout<<"essa letra e uma consoante \n";
+				}
+system("pause");
+return 0;
+					
+}
