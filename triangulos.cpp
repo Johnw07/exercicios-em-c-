f3b1e3518e2e550b6727d6e0f59fa6ba84b3cb4a
john mendes <131309978+Johnw07@users.noreply.github.com>
2024-04-16 19:03:20 -0300

    Update triangulos.cpp

--- triangulos.cpp
+++ triangulos.cpp
@@ -3,44 +3,44 @@ using namespace std;
 //joao washington 202303803206
 int main(){
 
-		int a,b,c, tot;
+int a,b,c, tot;
 
-			cout<<"digite o numero de triangulos \n";
-			cin>> tot;
+cout<<"digite o numero de triangulos \n";
+cin>> tot;
 			
-			for(int i=1; i<=tot; i++){
+for(int i=1; i<=tot; i++){
 			
-		cout<<"digite o primeiro lado \n";
-		cin>> a;
-		cout<<"digite o segundo lado \n";
-		cin>> b;
-		cout<<"digite o terceiro lado \n";
-		cin>> c;
+cout<<"digite o primeiro lado \n";
+cin>> a;
+cout<<"digite o segundo lado \n";
+cin>> b;
+cout<<"digite o terceiro lado \n";
+cin>> c;
 		
-			if(a<b+c && b<a+c && c<a+b){
+if(a<b+c && b<a+c && c<a+b){
 			
 		
 		
-				if(a==b && b==c ){
+   if(a==b && b==c ){
 		
-					cout<<"triangulo equilatero"<<endl;
+cout<<"triangulo equilatero"<<endl;
 		
-			}
-				else if(a==b || a==c || c==b){
+}
+else if(a==b || a==c || c==b){
 			
-					cout<<"triangulo isosceles"<<endl;
-			}
-				else{
-					cout<<"triangulo escaleno"<<endl;
-			}
+cout<<"triangulo isosceles"<<endl;
+}
+else{
+cout<<"triangulo escaleno"<<endl;
+}
 			
-			}
+}
 		
-				else{
+else{
 			
-					cout<<"os dados nao formam um triangulo \n";
-			}
-			}	
+cout<<"os dados nao formam um triangulo \n";
+}
+}	
 system("pause");
 return 0;
 }
