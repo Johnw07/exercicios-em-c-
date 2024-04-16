e524af78dd583493f75bb57042e1e634ce7be64f
john mendes <131309978+Johnw07@users.noreply.github.com>
2024-04-16 19:09:26 -0300

    Update calculadora trabalho.cpp

--- calculadora trabalho.cpp
+++ calculadora trabalho.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main()
 {
-    float num1, num2;
+float num1, num2;
 
     
-    cout << "Primeiro numero: ";
-    cin >> num1;
+cout << "Primeiro numero: ";
+cin >> num1;
 
-    cout << "Segundo numero: ";
-    cin >> num2;
+cout << "Segundo numero: ";
+cin >> num2;
 
     
-    cout << "Soma         : " << num1 + num2 << endl;
-    cout << "Subtracao    : " << num1 - num2 << endl;
-    cout << "Multiplicacao: " << num1 * num2 << endl;
-    cout << "Divisao      : " << num1 / num2 << endl;
-    cout << "Modulo       : " << (int)num1 % (int)num2 << endl;
-    cout << "Porcentagem  : " << 100.0*(num1/num2) << endl;
-    cout << "Media        : " << (num1 + num2)/2 << endl;
+cout << "Soma         : " << num1 + num2 << endl;
+cout << "Subtracao    : " << num1 - num2 << endl;
+cout << "Multiplicacao: " << num1 * num2 << endl;
+cout << "Divisao      : " << num1 / num2 << endl;
+cout << "Modulo       : " << (int)num1 % (int)num2 << endl;
+cout << "Porcentagem  : " << 100.0*(num1/num2) << endl;
+cout << "Media        : " << (num1 + num2)/2 << endl;
 
 
-    return 0;
+return 0;
 }
