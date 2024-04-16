cd66bc9b6bb483602575207e6f7a9cbb0c5f1da3
john mendes <131309978+Johnw07@users.noreply.github.com>
2024-04-16 19:08:26 -0300

    Update consoante e vogal.cpp

--- consoante e vogal.cpp
+++ consoante e vogal.cpp
@@ -6,17 +6,17 @@ int main(){
 char letra, consoante;
 	
 
-	cout<<"digite uma letra \n";
-	cin>>letra;
+cout<<"digite uma letra \n";
+cin>>letra;
 	
-	if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
-		cout<<"essa letra e uma vogal \n";	
-	}
-	else{
-		cout<<"essa letra e uma consoante \n";
-	}
+if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
+cout<<"essa letra e uma vogal \n";	
+}
+else{
+cout<<"essa letra e uma consoante \n";
+}
 	
-	system("pause");
-	return 0;
+system("pause");
+return 0;
 					
 }
